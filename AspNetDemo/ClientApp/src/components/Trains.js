59bffa2963fff5463d556d4a97862f959f0dcd07
Mikko Niemi <niemi.mikko.juhani@gmail.com>
2018-12-10 11:53:42 +0200

    Add train data fetch and display code
    First iteration for simple tests, after connection tests sample data is saved in text file and user for development purposes.

---
+++ AspNetDemo/ClientApp/src/components/Trains.js
@@ -0,0 +1,96 @@
+﻿import React, { Component } from 'react';
+
+export class Trains extends Component {
+    displayName = Trains.name
+
+    constructor(props) {
+        super(props);
+        this.state = { trains: [], loading: true };
+
+        Trains.renderPageControls();
+
+        //fetch('api/Train/GetTrainData')
+        //    .then(response => response.json())
+        //    .then(data => {
+        //        console.log(data);
+        //        let arr = [];
+        //        arr = data;
+        //        console.log(arr);
+        //        //this.setState({ trains: data, loading: false });
+        //    });
+    }
+
+    trainDataFetch() {
+        fetch('api/Train/GetTrainData')
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                let arr = [];
+                arr = data;
+                console.log(arr);
+                //Trains.state.trains = data;
+                //Trains.state.loading = false;
+                //Trains.state = { trains: data, loading: false };
+                this.setState({ trains: data, loading: false });
+            });
+    }
+
+    static renderPageControls() {
+        return (
+            <div className='trainControlsDiv'>
+                 
+            <table className='trainControls'>
+                <thead>
+                    <tr>
+                        <th>Valitse asema</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                          
+                    </tr>
+                </tbody>
+                </table>
+            </div>
+            );
+    }
+
+    static renderTrainTable(trains) {
+        return (
+            <table className='table'>
+                <thead>
+                    <tr>
+                        <th>Juna</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {trains.map(train =>
+                        <tr key={train.trainNumber}>
+                            <td>{train.trainNumber}</td>
+                            <td>{train.trainType}</td>
+                            <td>{train.trainCategory}</td>
+                        </tr>
+                    )}
+                </tbody>
+            </table>
+        );
+    }
+
+    render() {
+        let contents = this.state.loading
+            ? <p><em>Loading...</em></p>
+            : Trains.renderTrainTable(this.state.trains);
+
+        return (
+            <div>
+                <h1>Juna aikatauluja</h1>
+                <p>Hakee tietoja aseman lähtevistä ja saapuvista junista</p>
+                <div className='controlsDiv'>
+                    <button onClick={this.trainDataFetch.bind(this)}>Activate Lasers</button>
+                    {this.renderPageControls}
+                </div>
+                {contents}
+            </div>
+        );
+    }
+}
---
+++ AspNetDemo/Controllers/TrainController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetDemo.Controllers
+{
+    [Route("api/[controller]")]
+    public class TrainController : Controller
+    {
+        // GET: /<controller>/
+        [HttpGet("[action]")]
+        public string GetTrainData(string stationCode)
+        {
+
+            stationCode = "HKI";
+            string fixedUrl = "https://rata.digitraffic.fi/api/v1/live-trains/station/"
+                + stationCode + "?arriving_trains=1&departing_trains=1&include_nonstopping=false";
+            // Returns JSON string
+
+            //string data = GetTrains(fixedUrl);
+            string data = "";
+            StringBuilder sb = new StringBuilder();
+            try
+            {
+                //FileStream fs = new FileStream(@"E:\bootcamp\AspNetDemo\t.txt", FileMode.Open);
+
+                //long len = fs.Length;
+                //byte[] buffer = new byte[len];
+
+                //int count;                            // actual number of bytes read
+                //int sum = 0;                          // total number of bytes read
+
+                //// read until Read method returns 0 (end of the stream has been reached)
+                //while ((count = fs.Read(buffer, sum, (int)len - sum)) > 0)
+                //    sum += count;  // sum is a buffer offset for next reading
+                //StringBuilder sb = new StringBuilder();
+
+                //for (int i = 0; i < buffer.Length; i++)
+                //{
+                //    sb.Append(buffer[i].ToString());
+                //}
+                //data = sb.ToString();
+
+                //fs.Dispose();
+
+                using (StreamReader sr = new StreamReader(@"t.txt"))
+                {
+                    // Read the stream to a string, and write the string to the console.
+                    //String line = sr.ReadToEnd();
+                    //Console.WriteLine(line);
+                    sb.Append(sr.ReadToEnd());
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            data = sb.ToString();
+            //TrainData[] parsedData = JsonConvert.DeserializeObject<TrainData[]>(data);
+
+            return data;
+            //yield return parsedData;
+
+
+            
+        }
+
+        public class TrainReady
+        {
+            public bool accepted { get; set; }
+            public string source { get; set; }
+            public string timestamp { get; set; }
+        }
+
+        public class TrainTravelData
+        {
+            public string actualTime { get; set; }
+            public bool cancelled { get; set; }
+            public bool commercialStop { get; set; }
+            public string commercialTrack { get; set; }
+            public string countryCode { get; set; }
+            public int differenceInMinutes { get; set; }
+            public string scheduledTime { get; set; }
+            public string stationShortCode { get; set; }
+            public int stationUICCode { get; set; }
+            public TrainReady trainReady { get; set; }
+            public bool trainStopping { get; set; }
+            public string type { get; set; }
+            
+        }
+
+        public class TrainData
+        {
+            public int trainNumber { get; set; }
+            public string trainType { get; set; }
+            public string trainCategory { get; set; }
+            public string timetableType { get; set; }
+            public bool runningCurrently { get; set; }
+            public bool cancelled { get; set; }
+            public TrainTravelData[] timetableRows { get; set; }
+            public string commuterLineID { get; set; }
+            public int version { get; set; }
+        }
+
+        
+        string GetTrains(string url)
+        {
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(url);
+            try
+            {
+                WebResponse response = request.GetResponse();
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.UTF8);
+                    return reader.ReadToEnd();
+                }
+            }
+            catch (WebException ex)
+            {
+                WebResponse errorResponse = ex.Response;
+                using (Stream responseStream = errorResponse.GetResponseStream())
+                {
+                    StreamReader reader = new StreamReader(responseStream, System.Text.Encoding.GetEncoding("utf-8"));
+                    String errorText = reader.ReadToEnd();
+                    // log errorText
+                }
+                throw;
+            }
+        }
+    }
+}
